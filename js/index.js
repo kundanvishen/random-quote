20977d4b9ce7c43e58cdd1a9808c9381ed14500e
codak <kundan.singh979@gmail.com>
2017-09-29 02:17:53 +0000

    Add random background colors

--- js/index.js
+++ js/index.js
@@ -1,10 +1,38 @@
+var backgrounds = [{
+  "from": "#00bf8f",
+  "to": "#001510"
+},
+{
+  "from": "#43cea2",
+  "to": "#185a9d"
+},{
+  "from": "#FFA17F",
+  "to": "#00223E"
+},{
+  "from": "#360033",
+  "to": "#0b8793"
+},{
+  "from": "#948E99",
+  "to": "#2E1437"
+},{
+  "from": "#70e1f5",
+  "to": "#ffd194"
+},{
+  "from": "#556270",
+  "to": "#FF6B6B"
+},{
+  "from": "#780206",
+  "to": "#061161"
+}];
+
 var quoteContainer = $("#quoteContainer");
+
 
 $(document).ready(function(){
   $(document).on("click", "#newQuoteButton", function(){
     getQuote();
   })
-});
+}); // document.ready()
 
 function getQuote() {
   $.ajax({
@@ -17,6 +45,17 @@ function getQuote() {
     success: function(res) {
       $("#quoteContainer").html(res.quote);
       $("#author").html("~"+res.author);
+      var random = getRandomBackground();
+      $("html body").css({
+        "background": backgrounds[random].from,
+        "background": "linear-gradient(to right, "+backgrounds[random].from+", "+backgrounds[random].to+")",
+        "background": "-webkit-linear-gradient(to right, "+backgrounds[random].from+", "+backgrounds[random].to+")",
+        "background": "-webkit-linear-gradient(left, "+backgrounds[random].from+", "+backgrounds[random].to+")"
+      });
     }
   }); // ajax request
+} // getQuote()
+
+function getRandomBackground(){
+  var randomBackground = backgrounds[Math.floor(Math.random() * backgrounds.length)];
 }
